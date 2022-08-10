4c1fe3a9c81e5b30516a640057dbe166300bae60
bhupendra vaishnav <bhupendravaishnav9@gmail.com>
2022-08-10 14:00:57 +0530

    fourth

--- card/urls.py
+++ card/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path,include
 from .views import home
+from . import views
 urlpatterns = [
 
-    path('',home)
+    path( '',views.home ,name='home')
 ]
